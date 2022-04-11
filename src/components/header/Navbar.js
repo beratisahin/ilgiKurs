856043df543701b2986a328585bb66ca16a39e53
beratisahin <beratisahin94@gmail.com>
2022-04-11 21:06:23 +0300

    test2

---
+++ src/components/header/Navbar.js
@@ -0,0 +1,133 @@
+import "./Navbar.css";
+import React, { Component } from "react";
+
+import { Menubar } from 'primereact/menubar';
+
+import BusinessIcon from '@material-ui/icons/Business';
+import CameraAltIcon from '@material-ui/icons/CameraAlt';
+import ContactMailIcon from '@material-ui/icons/ContactMail';
+import HomeIcon from '@material-ui/icons/Home';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import AccountTreeIcon from '@material-ui/icons/AccountTree';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import MoodIcon from '@material-ui/icons/Mood';
+import İlgiLogo from "../header/İlgiMainLogo.png";
+export default class Navbar extends Component {
+  componentDidMount () {
+    const script = document.createElement("script");
+
+  
+      const hamburger = document.querySelector(".hamburger");
+      hamburger.addEventListener("click", function () {
+      this.classList.toggle("close");
+    });
+  
+
+    document.body.appendChild(script);
+}
+  render() {
+    
+    return (
+    
+         <header>
+          <nav class="navbar">
+            <div class="branding">
+              <a href="/anasayfa">
+                <img  className="MedisamLogo" title="Medisam Ağız ve Diş Sağlığı Polikliniği" src={İlgiLogo}></img>
+              </a>
+                
+            </div>
+            <label for="input-hamburger" class="hamburger "></label>
+            <input type="checkbox" id="input-hamburger" hidden></input>
+        <ul class="menu">
+          <li><a href="/" class="menu-link" style={{fontSize:"medium"}}><HomeIcon/>&nbsp;Ana Sayfa</a></li>
+          <li class="has-dropdown">
+            <a href="#" class="menu-link" style={{fontSize:"medium"}}><BusinessIcon/>&nbsp;Kurumsal &nbsp;
+              <span class="arrow"></span>
+            </a>
+            <ul class="submenu">
+            
+            <li><a href="/hakkimizda" class="menu-link">Hakkımızda</a></li>
+            <li><a href="/belgelerimiz" class="menu-link">Belgelerimiz</a></li>
+            <li><a href="/hekimlerimiz" class="menu-link">Hekimlerimiz</a></li>
+            <li><a href="/referanslarimiz" class="menu-link">Referanslarımız</a></li>
+
+              
+              
+            </ul>
+          </li>
+
+          
+          <li class="has-dropdown">
+            <a href="#" class="menu-link" style={{fontSize:"medium"}}><LocalHospitalIcon/>&nbsp;Hizmetlerimiz &nbsp;
+              <span class="arrow"></span>
+            </a>
+            <ul class="submenu">
+
+            <li class="has-dropdown">
+                <a class="menu-link">Ağız ve Diş Sağlığı &nbsp;
+                  <span class="arrow"></span>
+                </a>
+                <ul class="submenu">
+                 
+                  
+                  <li class="has-dropdown">
+                    <a class="menu-link">Ağız Diş ve Çene Cerrahisi&nbsp;
+                      <span class="arrow"></span>
+                    </a>
+                    <ul class="submenu">
+                      <li><a href="/agiz-dis-sagligi/agiz-dis-ve-cene-cerrahisi/yirmi-yas-dis-tedavisi" class="menu-link">20 Yaş Diş Tedavisi</a></li>
+                      <li><a href="/agiz-dis-sagligi/agiz-dis-ve-cene-cerrahisi/gomulu-dis-operasyonlari" class="menu-link">Gömülü Diş Operasyonları</a></li>
+                      <li><a href="/agiz-dis-sagligi/agiz-dis-ve-cene-cerrahisi/kist-ve-tumor-operasyonlari" class="menu-link">Kist ve Tümör Operasyonları</a></li>
+                      <li><a href="/agiz-dis-sagligi/agiz-dis-ve-cene-cerrahisi/cene-eklemi-rahatsizliklari" class="menu-link">Çene Eklemi Rahatsızlıkları</a></li>
+
+                    </ul>
+                  </li>
+                  <li><a href="/agiz-dis-sagligi/implant" class="menu-link">İmplant</a></li>
+                  <li><a href="/agiz-dis-sagligi/protetik-dis-tedavisi" class="menu-link">Protetik Diş Tedavisi</a></li>
+                  <li><a href="/agiz-dis-sagligi/zirkonyum-dis-kaplama" class="menu-link">Zirkonyum Diş Kaplama</a></li>
+                  <li><a href="/agiz-dis-sagligi/porselen-lamine" class="menu-link">Porselen Lamine</a></li>
+                  <li><a href="/agiz-dis-sagligi/dis-beyazlatma" class="menu-link">Diş Beyazlatma</a></li>
+                  <li><a href="/agiz-dis-sagligi/ortodonti" class="menu-link">Ortodonti (Tel Tedavisi)</a></li>
+                  <li><a href="/agiz-dis-sagligi/pedodonti" class="menu-link">Çocuk Diş Hekimliği (Pedodonti)</a></li>
+                  <li><a href="/agiz-dis-sagligi/kanal-ve-dolgu-tedavisi" class="menu-link">Kanal ve Dolgu Tedavisi</a></li>
+                  <li><a href="/agiz-dis-sagligi/periodontoloji" class="menu-link">Diş Eti Tedavisi (Periodontoloji)</a></li>
+                  <li><a href="/agiz-dis-sagligi/dijital-dis-hekimligi" class="menu-link">Dijital Diş Hekimliği</a></li>
+                 
+                </ul>
+              </li>
+
+              <li><a href="/lazer-epilasyon-ve-guzellik-merkezi" class="menu-link">Güzellik Merkezi</a></li>
+              <li><a href="/sac-ekim-merkezi" class="menu-link">Saç Ekim Merkezi</a></li>
+              
+            </ul>
+          </li>
+          <li><a href="/galeri" class="menu-link" style={{fontSize:"medium"}}><CameraAltIcon/>&nbsp;Galeri</a></li>
+         
+          <li class="has-dropdown">
+            <a href="#" class="menu-link" style={{fontSize:"medium"}}><AccountTreeIcon/>&nbsp;Şubelerimiz &nbsp;
+              <span class="arrow"></span>
+            </a>
+            <ul class="submenu">
+            
+            <li><a href="/merzifon-dis" class="menu-link">Merzifon Diş Polikliniği</a></li>
+            <li><a href="/vezirkopru-dis" class="menu-link">Vezirköprü Diş Polikliniği</a></li>
+              
+              
+            </ul>
+          </li>
+          <li><a href="/iletisim" class="menu-link" style={{fontSize:"medium"}}><ContactMailIcon/>&nbsp;İletişim</a></li>
+        </ul>
+
+
+
+          </nav>
+        </header>
+
+          
+   
+      
+     
+    );
+  }
+}
---
+++ src/components/home/Home.js
@@ -0,0 +1,74 @@
+import React from "react";
+import ReactPlayer from "react-player";
+import "./Home.css";
+import NedenBercislina from "../home/neden/Neden.js";
+import Mail from "../iletisim/ContactForm";
+import Slider from "../slider/Slider";
+import DoktorOwl from "../doktorlar/DoktorOwl.js";
+import Klinik from "../klinik/Klinik.js";
+
+import testimg from "../header/MedisamYuvarlakLogo.png"
+import Test from "../test/Test";
+
+
+import TopluHizmet from "../topluhizmet/TopluHizmet.js";
+
+
+function Home() {
+  return (
+    <div>
+ 
+      <div className="col-12 homepageSlider" style={{width:"80%",justifyContent:"center",margin:"auto"}}>
+        <p className="acilDurumMobil" style={{display:"none"}}>Kayıtlarımız ücretsizdir.</p>
+         <Slider/>  
+      </div>
+      
+      <div className="col-12">
+            <NedenBercislina/>
+      </div>
+      
+      <br/>
+      <hr/>
+      <div className="row">
+        <TopluHizmet/>
+      </div>
+        
+
+      <hr/>
+      <div className="col-12">
+        <h2 className="doktorlarTitle" style={{textAlign:"center",fontSize:"x-large",color:"#3d667c",fontFamily: "'Times New Roman', Times, serif;"}}>Doktorlarımız</h2>
+         <DoktorOwl/>
+      </div>
+      <br/>
+      <hr/>
+      <div className="col-12">
+      <h2 className="doktorlarTitle" style={{textAlign:"center",fontSize:"x-large",color:"#3d667c",fontFamily: "'Times New Roman', Times, serif;"}}>Kliniklerimiz</h2>
+         <Klinik/>
+      </div>
+     
+      
+      <br/>
+      <hr/>
+      
+      <div className="col-12">
+            <Mail/>
+      </div>
+      <br/>
+      <div className="row m-2">
+         
+
+          <div className="col-lg-12  col-12 harita" style={{marginTop:"2.5rem"}}>
+                    <p className="harita_baslik">Lokasyonumuz</p>
+
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3041.1123751275436!2d36.53736451539363!3d40.33985307937422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x407db9fbf021f42f%3A0x77e8eb8ff325ac2c!2zQWx0xLF5w7x6ZXZsZXIsIMSwbWFtbMSxayBDZC4gTm86NCwgNjAyMDAgVG9rYXQgTWVya2V6L1Rva2F0!5e0!3m2!1str!2str!4v1649692137802!5m2!1str!2str" 
+                className="googleharita" style={{width:"100%", height:"45rem", frameBorder:"0", allowFullScreen:"", ariaHidden:"false", tabIndex:"0",border:"2px #2E6772 solid",paddingBottom:"0.5rem",paddingTop:"0.5rem"}}> </iframe>
+                
+          </div> 
+
+      </div>
+
+    </div>
+  );
+}
+
+export default Home;
