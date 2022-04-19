528852a58bc1cedee571fc64561c704c8061af74
beratisahin <beratisahin94@gmail.com>
2022-04-19 18:50:29 +0300

    v2.2

--- src/components/klinik/Klinik.js
+++ src/components/klinik/Klinik.js
@@ -5,11 +5,15 @@ import 'owl.carousel/dist/assets/owl.carousel.min.css'
 import 'owl.carousel/dist/assets/owl.theme.default.min.css'
 import { NavigateBeforeSharp } from '@material-ui/icons';
 
+import r1 from "./i1.jpeg";
 import r2 from "./i2.jpeg";
 import r3 from "./i3.jpeg";
 import r4 from "./i4.jpeg";
+import r5 from "./i5.jpeg";
 import r6 from "./i6.jpeg";
+import r7 from "./i7.jpeg";
 import r8 from "./i8.jpeg";
+import r9 from "./i9.jpeg";
 import r11 from "./i11.jpeg";
 
 
@@ -50,7 +54,11 @@ export default function Klinik() {
     return (
        
              <OwlCarousel className="owl-theme owl-roof" {...options}>
-                 
+                 <div className="item owl-slider">
+                    <div className="owlCarouselTempKlinik">
+                        <img src={r1} className="owlCarouselImageKlinik" alt="Tokat İlgi Kurs Merkezi" title="Tokat İlgi Kurs Merkezi"></img>
+                    </div>
+                </div>
                 <div className="item owl-slider">
                     <div className="owlCarouselTempKlinik">
                         <img src={r2} className="owlCarouselImageKlinik" alt="Tokat İlgi Kurs Merkezi" title="Tokat İlgi Kurs Merkezi"></img>
@@ -68,12 +76,27 @@ export default function Klinik() {
                 </div>
                 <div className="item owl-slider">
                     <div className="owlCarouselTempKlinik">
+                        <img src={r5} className="owlCarouselImageKlinik" alt="Tokat İlgi Kurs Merkezi" title="Tokat İlgi Kurs Merkezi"></img>
+                    </div>
+                </div>
+                <div className="item owl-slider">
+                    <div className="owlCarouselTempKlinik">
                         <img src={r6} className="owlCarouselImageKlinik" alt="Tokat İlgi Kurs Merkezi" title="Tokat İlgi Kurs Merkezi"></img>
                     </div>
                 </div>
                 <div className="item owl-slider">
                     <div className="owlCarouselTempKlinik">
+                        <img src={r7} className="owlCarouselImageKlinik" alt="Tokat İlgi Kurs Merkezi" title="Tokat İlgi Kurs Merkezi"></img>
+                    </div>
+                </div>
+                <div className="item owl-slider">
+                    <div className="owlCarouselTempKlinik">
                         <img src={r8} className="owlCarouselImageKlinik" alt="Tokat İlgi Kurs Merkezi" title="Tokat İlgi Kurs Merkezi"></img>
+                    </div>
+                </div>
+                <div className="item owl-slider">
+                    <div className="owlCarouselTempKlinik">
+                        <img src={r9} className="owlCarouselImageKlinik" alt="Tokat İlgi Kurs Merkezi" title="Tokat İlgi Kurs Merkezi"></img>
                     </div>
                 </div>
                 <div className="item owl-slider">
